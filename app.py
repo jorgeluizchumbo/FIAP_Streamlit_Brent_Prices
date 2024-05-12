cdb47f84d89352e253ec6aff6ee93765970d54c5
Jorge Luiz Chumbo <108941692+jorgeluizchumbo@users.noreply.github.com>
2024-05-12 19:02:02 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import altair as alt
+import joblib
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 from sklearn.model_selection import train_test_split
