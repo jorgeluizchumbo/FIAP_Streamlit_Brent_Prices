2071be0e7ccb8580adb9bb6b4c6b3053187ab05f
Jorge Luiz Chumbo <108941692+jorgeluizchumbo@users.noreply.github.com>
2024-05-12 18:43:11 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
 ########### Importação das bibliotecas ########### 
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-import numpy as np
-import altair as alt
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 from sklearn.model_selection import train_test_split
